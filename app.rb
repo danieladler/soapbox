9c3e9978325c8a0e4b961e6206cbde46ba7ee8da
Daniel Adler <weston.adler@gmail.com>
2015-10-14 11:19:26 -0700

    step 6, can loop multiple Bubbles or exit

--- app.rb
+++ app.rb
@@ -1,48 +1,58 @@
 require_relative 'bubble'
 
-# 6. Start over at step 2.
-
 all_bubbles = [] #is the very top of the app the best place for this?
 
 # 1. Ask for a username
 puts "Please enter username"
 input = gets.chomp
 username = "@"+input
-puts "Welcome to Soapbox, " + username + "!"
+puts "\nWelcome to Soapbox, " + username + "!"
 
-# 2. Ask for the body of a Bubble
-puts "Enter text for your Bubble:"
-input = gets.chomp
-bubble_text = input
-bubble_time = Time.now #later, figure out how to format time
-current_user = username
+# 6. Start over at step 2.
+while true
+  # 2. Ask for the body of a Bubble
+  puts "\nEnter text for your Bubble or 'exit':"
+  input = gets.chomp
+  bubble_text = input
+  bubble_time = Time.now #later, figure out how to format time
+  current_user = username
 
-# puts "#{current_user} said #{bubble_text} at #{bubble_time}"
-# ^^^ test for user input coming through - it does!
-# ^^^ But it's not yet a Bubble itself - just a bunch of strings
+    # puts "#{current_user} said #{bubble_text} at #{bubble_time}"
+    # ^^^ test for user input coming through - it does!
+    # ^^^ But it's not yet a Bubble itself - just a bunch of strings
 
-single_bubble = {username: current_user, body: bubble_text, created_at: bubble_time}
-# ^^^ now user input is in a hash
+  if input == "exit"
+    puts "\nThanks for using SoapBox!"
+    exit
 
-# 3. Create a Bubble instance using that information (with created_at being Time.now)
-bubble_instance = Bubble.new(single_bubble)
-# puts bubble_instance.class # <-- now user input exists as an instance of Bubble!
+  else
 
-# puts test_bubble_instance.first
-# ^^having trouble pulling attributes out of this instance of Bubble.
+    single_bubble = {username: current_user, body: bubble_text, created_at: bubble_time}
+    # ^^^ now user input is in a hash
 
-# 4. Add the bubble instance to an Array
-all_bubbles << bubble_instance
+    # 3. Create a Bubble instance using that information (with created_at being Time.now)
+    bubble_instance = Bubble.new(single_bubble)
+    # puts bubble_instance.class # <-- now user input exists as an instance of Bubble!
 
-# puts "all_bubbles empty?"
-# puts all_bubbles.empty? # <-- false -- so it's populated
-# puts "all_bubbles class?"
-# puts all_bubbles.class # <-- array
-# puts "all_bubbles contents - class?"
-# puts all_bubbles.first.class # <-- Bubble
+    # puts test_bubble_instance.first
+    # ^^having trouble pulling attributes out of this instance of Bubble.
 
-# 5. Print out the details of each bubble.
-all_bubbles.each do |b|
-  puts "\t#{b.username} squeaks:\n\t'#{b.body}'"
-  puts "\tsqueaked at: #{b.created_at}"
+    # 4. Add the bubble instance to an Array
+    all_bubbles << bubble_instance
+
+    # puts "all_bubbles empty?"
+    # puts all_bubbles.empty? # <-- false -- so it's populated
+    # puts "all_bubbles class?"
+    # puts all_bubbles.class # <-- array
+    # puts "all_bubbles contents - class?"
+    # puts all_bubbles.first.class # <-- Bubble
+
+    # 5. Print out the details of each bubble.
+    puts "\n#{username}'s Soapbox history"
+    all_bubbles.each do |b|
+      puts
+      puts "\t#{b.username} squeaks:\n\t'#{b.body}'"
+      puts "\tsqueaked at: #{b.created_at}" #reformat to shorter time display?
+    end
+  end
 end
