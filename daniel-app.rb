c09625de69ebec1af036fc43f1ca6e35586fbdc6
Daniel Adler <weston.adler@gmail.com>
2015-10-15 12:07:14 -0700

    tweets write to Dropbox folder, tweet=filename, username=contents

--- daniel-app.rb
+++ daniel-app.rb
@@ -36,7 +36,13 @@ while true
 
     bubble_instance = Bubble.new(single_bubble)
 
+    file =  File.open("/Users/danieladler/Dropbox/SoapBox/#{single_bubble[:body]}.txt","w")
+    file.write "#{single_bubble[:username]}"
+    file.close
+
     all_bubbles << bubble_instance
+    # keeping array so that it also populates array for 'history' function
+    # but perhaps there's also a way to pull history of my tweets from the actual files?
 
   elsif input.downcase == "exit"
     puts "\nThanks for using " + "SoapBox".on_light_blue + "!"
--- daniel-bubble.rb
+++ daniel-bubble.rb
@@ -1,11 +1,17 @@
 class Bubble
   attr_accessor :username,
                 :body,
-                :created_at
+                :created_at,
+                :file #new
 
   def initialize (b)
     @username = b[:username]
     @body = b[:body]
     @created_at = Time.now.strftime("%A, %d %b %Y %l:%M %p")
   end
+
+  def save_file(b) #argument could be filepath of where file should be saved
+    @file = File.open(@body.txt, "w")
+  end
+
 end
