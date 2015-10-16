002d57409335a4685c7be698337d6c68864b439b
Daniel Adler <weston.adler@gmail.com>
2015-10-16 11:48:53 -0700

    added feed, reverse-sorted by creation time (newest at top)

--- daniel-app.rb
+++ daniel-app.rb
@@ -1,3 +1,13 @@
+# TO-DO:
+# * put bubble formatter in Bubble class file
+# * improve feed formatting
+# * implement refresh of feed
+# * eliminate 'history' option & section (including all_bubbles array) once
+#   feed is in place.
+
+#commit notes:
+# *
+
 require_relative 'daniel-bubble'
 require 'colorize'
 
@@ -20,9 +30,10 @@ else
   puts "\nWelcome to " + "SoapBox".on_light_blue + ", " + username.light_yellow.bold + "!"
 end
 
-while true
+while true #eliminate 'history' feature once 'feed' is in place
   puts """\nStart typing to squeak your next Bubble
-  -type 'history' for past squeaks
+  -type 'feed' for all Bubbles across Soapbox
+  -type 'history' for past Bubbles
   -type 'exit' to quit
   """
   puts "#{username.light_yellow} squeaks:".on_light_black
@@ -31,7 +42,7 @@ while true
   bubble_time = Time.now
   current_user = username
 
-  if input.downcase != "exit" && input.downcase != "history"
+  if input.downcase != "exit" && input.downcase != "history" && input.downcase != "feed"
     single_bubble = {username: current_user, body: bubble_text, created_at: bubble_time}
 
     bubble_instance = Bubble.new(single_bubble)
@@ -55,5 +66,107 @@ while true
       puts "\tsqueaked at: ".bold + "\t" + "#{b.created_at}"
       puts "\t"+"_ _".bold*15
     end
+
+  elsif input.downcase == "feed"
+
+    # the feed (Dropbox folder) is the source of all necessary data
+    # create new Bubbles here by walking through the folder
+
+    entire_feed = Dir.glob "/Users/danieladler/Dropbox/SoapBox/*"
+    puts "Total # of posts:"
+    puts entire_feed.count
+
+    # FEED OPTION 1
+    # entire_feed.each do |file|
+    #   target_file = File.read(file)
+    #   puts target_file
+    #   puts "- -"*10
+    #   # username = target_file.read
+    #   # created_at = target_file.birthtime
+    #
+    #     # target_file_hash = {
+    #     #   # body:
+    #     #   username: target_file.read,
+    #     #   created_at: target_file.birthtime #+ strftime
+    #     # }
+    #     #
+    #     # feed_bubble_instance = Bubble.new(target_file_hash)
+    #     #
+    #     # # new bubble formatting
+    #     # # close file?
+    #     # File.close(file)
+    #
+    # end
+
+    # FEED OPTION 2
+    time_sorted_feed = entire_feed.sort_by {|x| File.birthtime(x)}.reverse
+    time_sorted_feed.each do |file|
+      single_feed_file = File.open(file)
+      contents = {
+        body: File.basename(single_feed_file),
+        username: File.read(single_feed_file),
+        created_at: single_feed_file.birthtime #+ strftime
+      }
+
+      # formatted_content =
+      # """#{contents[:username].bold.light_yellow}:\t#{contents[:body]}
+      # \nsqueaked at #{contents[:created_at]}--------------------------
+      # """
+
+      # formatted_bubble = """
+      #    \t#{contents[:username].bold.light_yellow}: + \t + #{contents[:body]}
+      #    \tsqueaked at:.bold \t + #{b.created_at}
+      #    \t+_ _.bold*15
+      #   """
+
+      feed_bubble_instance = Bubble.new(contents)
+
+      puts feed_bubble_instance  # <-- actually, need to puts each new bubble!
+
+    end
+
+    # {|f| puts f}
+    # ^^^ this globs target dir
+    # ^^^ it's an array of strings, which are all directories w/ body @ end
+    # ^^^ e.g. /Users/danieladler/Dropbox/SoapBox/asdsa
+
+    # all_bubble_handles = all_bubble_bodies.map {|f| File.read(f)}
+
+
+    # all_file_contents = all_bubbles.map {|f| f + File.read(f)} # <--
+    # # ^^^ this variable is also an array, made up of strings that are
+    # puts all_file_contents[0]
+    # puts all_file_contents[2]
+    # puts all_file_contents[4]
+    # puts all_file_contents[7]
+    # puts all_file_contents[9]
+    # puts all_file_contents[11]
+
+
+    # do the mapping and formatting in one step - while creating the bubble
+    # VVVVV when creating new bubble, try with argument pulling from the glob'd directory
+    # feed_bubble_instance = all_bubble_locs.each do |f|
+    #     username: File.read(f)
+    #     #body: all_bubble_contents.each {|b| (b)} # chop off the end of the directories
+    #     # created_at:                             # TBD
+    # }
+    # )
+    # end
+    # puts feed_bubble_instance.class
+    # feed_bubble_instance.class
+
+    # OLD CODE??
+    # [feed_single_bubble] = {
+    #     username: current_user,
+    #     body: bubble_text,
+    #     created_at: bubble_time
+    #   }
+
+    # give prominent place to 'refresh' function of feed as I'll want it to
+    # stand out against other user options (not sure want to rewrite loops to
+    # make feed a 'different place' in user flow)
+
   end
 end
+
+# end
--- daniel-bubble.rb
+++ daniel-bubble.rb
@@ -1,3 +1,11 @@
+# TO DO:
+# *~ create class method for Bubbles -- read whole feed. include refresh inside?
+    # def self.read_all
+    #  Dir.glob("*")
+    # end
+
+# *~ also set class method for read_all_specific_user? -- read whole feed of specific users
+
 class Bubble
   attr_accessor :username,
                 :body,
@@ -10,8 +18,16 @@ class Bubble
     @created_at = Time.now.strftime("%A, %d %b %Y %l:%M %p")
   end
 
-  def save_file  File.open("/Users/danieladler/Programming/Code_Builders/ruby_practice/projects/soapbox/bubble_test/#{@body}", "w") do |file|
+  def save_file
+
+    File.open("/Users/danieladler/Programming/Code_Builders/ruby_practice/projects/soapbox/bubble_test/#{@body}", "w") do |file|
       file << "#{@username}"
     end
   end
 end
+
+#write folder for finished app: /Users/danieladler/Dropbox/SoapBox/
+
+
+#write folder for testing
+# /Users/danieladler/Programming/Code_Builders/ruby_practice/projects/soapbox/bubble_test/
